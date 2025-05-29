4d21adfa70858464c218b322bc5404769021eec6
Haskell <haskell@MacBook-Pro-lain.local>
2025-05-29 14:49:02 +0300

    added optional arguments for output file and media dir

--- args.py
+++ args.py
@@ -4,4 +4,7 @@ def parse():
     parser = argparse.ArgumentParser(description="CLI to dump telegram channel data.")
     parser.add_argument('api_id', type=str, help='telegram API id.')
     parser.add_argument("api_hash",type=str,help="telegram API hash.")
+    parser.add_argument("--media", "--m",help="media directory", default="media")
+    parser.add_argument("--output","--o",help="output file",default="output.jsonl")
+    
     return parser.parse_args()
