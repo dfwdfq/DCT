bd68ae97457e0470e3cb3928c049a696101b9c11
Haskell <haskell@MacBook-Pro-lain.local>
2025-05-29 14:06:24 +0300

    use a la less behaviour to list channels

--- interface.py
+++ interface.py
@@ -1,6 +1,9 @@
 import readline
 import args
+import pydoc
+import pandas as pd
 from dumper import Dumper
+from tabulate import tabulate
 
 class Interface:
     def __init__(self):
@@ -10,15 +13,34 @@ class Interface:
         except KeyboardInterrupt as _:
             print("quit...")
 
-        self.commands = {}
+        self.commands = {"list":self._list_channels}
         self.channels = []
 
-    def _run(self):
+    def _list_channels(self,args:list) -> None:
+        """list available channels."""
+        self.channels = self.d.list_available_channels()
+        data = {
+            "name":[],
+            "telegram id":[]
+        }
+        counter = 0
+        for ch in self.channels:
+            data["name"].append(ch["title"])
+            data["telegram id"].append(ch["id"])
+            counter += 1
+            
+        df = pd.DataFrame(data)
+        table = tabulate(df, headers='keys', tablefmt='psql')
+        pydoc.pager(table)
+        
+    def _run(self) -> None:
         head,*tail = input(">>").split()
         if head not in self.commands.keys():
             print("dtc: error: no command '{}'!".format(head))
+        else:
+            self.commands[head](tail)
         
-    def run(self):
+    def run(self) -> None:
         while True:
             try:
                 self._run()
