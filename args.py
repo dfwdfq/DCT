a605598cbd54815adcf2efc9d750d40a31abf85e
Haskell <haskell@MacBook-Pro-lain.local>
2025-05-29 15:14:02 +0300

    added optional argument to use session file

--- args.py
+++ args.py
@@ -6,5 +6,6 @@ def parse():
     parser.add_argument("api_hash",type=str,help="telegram API hash.")
     parser.add_argument("--media", "--m",help="media directory", default="media")
     parser.add_argument("--output","--o",help="output file",default="output.jsonl")
+    parser.add_argument("--session","--s",help="session file",default="")
     
     return parser.parse_args()
